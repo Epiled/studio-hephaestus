184586cb9b29559780a0fee525201cd28b0645c2
Felipe De Andrade <felipe.deam98@gmail.com>
2024-02-20 01:37:13 -0300

    Remoção de imports nãu usados

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import EstilosGlobais from './styles/EstilosGlobais'
 import Home from './pages/Home'
-
 
 const App: React.FC = () => {
 
